d9b0ddc385b08e7327fed1ce55d96f8c16b521ae
Ethel C <cofresi.ethel@gmail.com>
2018-09-05 13:09:53 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -18,6 +18,7 @@ connection = ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
+
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS artists (
   id INTEGER PRIMARY KEY,
@@ -26,8 +27,6 @@ sql = <<-SQL
   age INTEGER,
   hometown TEXT
   )
-SQL
-
 ActiveRecord::Base.connection.execute(sql)
 
 require_relative "../artist.rb"
